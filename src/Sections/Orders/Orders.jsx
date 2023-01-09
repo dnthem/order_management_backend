f5307472e58cfea7142dcce3df80b79c017b1712
Them Dang <dnthem@gmail.com>
2023-01-08 21:14:57 -0800

    update orders - save - undo

--- src/Sections/Orders/Orders.jsx
+++ src/Sections/Orders/Orders.jsx
@@ -2,6 +2,7 @@ import Header from "../../components/Header";
 import {FaUndoAlt} from "react-icons/fa";
 import {AiOutlineCheckCircle} from "react-icons/ai";
 import {BiDownload} from "react-icons/bi";
+import {FiSave} from "react-icons/fi";
 import { useEffect, useState } from "react";
 import MenuTable from "./MenuTable";
 import OrderTable from "./OrderTable";
@@ -9,31 +10,47 @@ import indexedDBController from "../../indexedDB/indexedDB";
 import { GetDataBaseContext } from "../../App";
 
 
-function updateOrder(order, newItem) {
+function updateOrder(order, item) {
     let resOrder = {...order};
-    const idx = resOrder.ItemKeys.findIndex(e => e === newItem.id);
+    const idx = resOrder.ItemKeys.findIndex(e => e === item.id);
     if (idx >= 0) {
         resOrder.Quantities[idx]++;
         resOrder.Totals[idx] += resOrder.Prices[idx];
     } else {
-        resOrder.ItemKeys.push(newItem.id);
-        resOrder.ItemNames.push(newItem.Title);
-        resOrder.Prices.push(newItem.Price);
+        resOrder.ItemKeys.push(item.id);
+        resOrder.ItemNames.push(item.Title);
+        resOrder.Prices.push(item.Price);
         resOrder.Quantities.push(1);
-        resOrder.Totals.push(newItem.Price);
+        resOrder.Totals.push(item.Price);
     }
     
     return resOrder
+}
+
+function undoOrder(order, item) {
+    let resOrder = {...order};
+    const idx = resOrder.ItemKeys.findIndex(e => e === item.id);
+    if (--resOrder.Quantities[idx] === 0) {
+        resOrder.ItemKeys.splice(idx ,1);
+        resOrder.ItemNames.splice(idx, 1);
+        resOrder.Prices.splice(idx,1);
+        resOrder.Quantities.splice(idx,1);
+        resOrder.Totals.splice(idx,1);
+    } else {
+        resOrder.Totals[idx] -= item.Price;
+    }
+    return resOrder;
 }
 
 function Orders(props) {
     const {db} = GetDataBaseContext()
     const [date, setDate] = useState(new Date().toLocaleDateString('en-us'))
     const [total, setTotal] = useState(0);
+    const [undoStack, setUndoStack] = useState([]);
     const [menu, setMenu] = useState([]);
     const [order, setOrder] = useState(
         {
-            date: new Date().toLocaleDateString('en-us'),
+            Date: new Date().toLocaleDateString('en-us'),
             ItemKeys: [],
             ItemNames: [],
             Prices: [],
@@ -46,9 +63,30 @@ function Orders(props) {
         // Add item to orders
         console.log(key)
         // update order
-        const newOrder = updateOrder(order, menu.find(e => e.id === key))
+        const item =  menu.find(e => e.id === key)
+        const newOrder = updateOrder(order, item)
+        setUndoStack([...undoStack, item]);
         setOrder(newOrder);
         setTotal(newOrder.Totals.reduce((acc, curr) => acc + curr, 0))
+    }
+
+    const handleUndo = () => {
+        const item = undoStack.pop();
+        
+        const newOrder = undoOrder(order, item);
+        setOrder(newOrder);
+        setUndoStack(undoStack);
+        setTotal(total - item.Price);
+    }
+
+    const handleSave = async () => {
+        try {
+            await indexedDBController.updateARecord(db, 'Orders', order)
+            alert ('Update order successful')
+            setUndoStack([]);
+        } catch (error) {
+            alert (error)
+        }
     }
     
     useEffect(() => {
@@ -75,7 +113,8 @@ function Orders(props) {
                 </div>
                 <div className="col-4">
                     <div className="d-flex justify-content-between">
-                        <button className="mt-4 btn">Undo <FaUndoAlt/></button>
+                        <button className="mt-4 btn" onClick={handleSave}>Save <FiSave/></button>
+                        <button className="mt-4 btn" onClick={handleUndo}  disabled={undoStack.length===0? true:false}>Undo <FaUndoAlt/></button>
                         <button className="mt-4 btn">Complete <AiOutlineCheckCircle/></button>
                         <button className="mt-4 btn">Save to local <BiDownload/></button>
                     </div>
