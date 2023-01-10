1f43bf539d55a764b11136c0881bf14389d3e1b3
Them Dang <dnthem@gmail.com>
2023-01-09 16:31:39 -0800

    udpate

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,49 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import { VitePWA } from 'vite-plugin-pwa'
+import mkcert from 'vite-plugin-mkcert'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: true
+    host: true,
+    https: true
   },
-  plugins: [react()],
+  plugins: [react(),
+    mkcert(),
+    VitePWA({ 
+      registerType: 'autoUpdate',
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      },
+      includeAssets: ['icon_512x512.png', 'icon_180x180.png', 'favicon.ico'],
+      manifest: {
+        name: 'Order Management',
+        short_name:'Order Management',
+        description: 'Order Management and analytics',
+        theme_color: '#212529',
+        icons: [
+          {
+            src: 'icon_192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icon_512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icon_512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ],
+      }
+     })
+  ],
 })
