b56ad37b72412c43e52881bc5c2c58625832457a
Them Dang <dnthem@gmail.com>
2023-01-09 23:26:54 -0800

    update

--- src/components/MenuTableCard.jsx
+++ src/components/MenuTableCard.jsx
@@ -1,8 +1,10 @@
+import imgTemplate from '../assets/template.jpg'
+
 function MenuTableCard(props) {
     return ( 
         <tr>
             <td style={{verticalAlign:'middle'}}>
-                <img src={props.Photo !== undefined? URL.createObjectURL(props.Photo):'/template.jpg'} alt="" width={'100vw'} /> {props.Title}
+                <img src={props.Photo !== undefined? URL.createObjectURL(props.Photo):imgTemplate} alt="" width={'100vw'} /> {props.Title}
             </td>
             <td style={{verticalAlign:'middle'}}>{props.Price}</td>
             <td colSpan={2}  style={{verticalAlign:'middle'}}><button className="btn" onClick={() => props.select(props.cardID)}>select</button></td>
