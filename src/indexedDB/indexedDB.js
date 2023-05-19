d5bc5d69c7d86bf6a97e4c3898a757c5ce544167
Them Dang <dnthem@gmail.com>
2023-05-19 11:25:59 -0700

    updates

--- src/indexedDB/indexedDB.js
+++ src/indexedDB/indexedDB.js
@@ -33,11 +33,10 @@ indexedDBController.createDB = function (dbName, version = undefined) {
       customers.createIndex('phone', 'phone', { unique: true });
 
       menu.createIndex('id', 'id', { unique: true });
-      // sampleData['OrdersV2'].forEach(e => orderV2.add(e));
+      sampleData['OrdersV2'].forEach(e => orderV2.add(e));
       sampleData['Menu'].forEach(e => menu.add(e));
-      // sampleData['Customers'].forEach(e => customers.add(e));
-      // sampleData['Orders'].forEach(e => order.add(e))
-      // sampleData['Income'].forEach(e => income.add(e))
+      sampleData['Customers'].forEach(e => customers.add(e));
+      sampleData['Income'].forEach(e => income.add(e))
     };
     request.onerror = (event) => reject(event.error);
 
