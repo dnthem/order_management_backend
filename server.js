621f103c67803847c48cd16bad7dfd3cf73828e4
Them Dang <dnthem@gmail.com>
2023-07-18 17:03:27 -0700

    update

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(path.resolve(), 'public')));
 
 app.use(cors({
-  origin: 'https://order-management-api.netlify.app',
+  origin: true,
 }));
 
 await db.connect();
