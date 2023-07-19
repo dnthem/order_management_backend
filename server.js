f180b79f1d0945695d5e58b9a210d1c1fc50309f
Them Dang <dnthem@gmail.com>
2023-07-18 17:10:57 -0700

    date

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(path.resolve(), 'public')));
 
 app.use(cors({
-  origin: true,
+  origin: 'https://order-management-api.netlify.app',
 }));
 
 await db.connect();
